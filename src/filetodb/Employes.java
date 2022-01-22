5bcbf6b57fff7fe0989ad814421404e68c73d95f
navigationhazard <knston@outlook.de>
2022-01-22 17:19:23 +0100

    commit

--- src/filetodb/Employes.java
+++ src/filetodb/Employes.java
@@ -111,18 +111,4 @@ public class Employes {
         }
     }
 }
-class FDemo {
 
-    public static void main(String[] args){
-
-        Employes list = new Employes();
-        try {
-            list.readData();
-            list.displayData();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-    }
-
-}
---
+++ src/filetodb/Main.java
@@ -0,0 +1,18 @@
+package filetodb;
+public class Main {
+   
+
+        public static void main(String[] args){
+    
+            Employes list = new Employes();
+            try {
+                list.readData();
+                list.displayData();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+    
+        }
+    
+    
+}
